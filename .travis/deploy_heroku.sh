0aff6daaa97d89bd167e248981c5210bbf1d6a6b
romainsacchi <r_s@me.com>
2019-10-12 16:09:15 +0200

    Update deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install @heroku-cli/plugin-container-registry
+heroku plugins:install @heroku-cli/plugin-container-registry --password=$HEROKU_API_KEY
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
