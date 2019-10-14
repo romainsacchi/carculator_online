cc5a319f22859b3ad52d257fb18539ed8e0bc28b
romainsacchi <r_s@me.com>
2019-10-14 18:03:06 +0200

    Added templates and static files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask App Project."""
 
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from carculator import *
 app = Flask(__name__)
 
@@ -9,19 +9,10 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
 
-    json_data = {'Hello': 'World'}
-    return jsonify(json_data)
 
-@app.route('/ttw')
-def get_ttw():
-    cip = CarInputParameters()
-    cip.static()
-    dcts, array = fill_xarray_from_input_parameters(cip)
-    cm = CarModel(array, cycle='WLTC')
-    cm.set_all()
-    ttw_energy = cm.array.sel(parameter='TtW energy', year=2017).values.tolist()
-    json_data = {'TtW energy': ttw_energy}
-    return jsonify(json_data)
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run()
