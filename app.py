d34208b49e0c12b543911754202b3aa8771df58b
romainsacchi <r_s@me.com>
2019-10-14 20:05:56 +0200

    Update requirements

--- app.py
+++ app.py
@@ -2,17 +2,13 @@
 
 from flask import Flask, render_template
 from carculator import *
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-
-
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run()
