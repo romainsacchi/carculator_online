6b34a35ff4a6bcc420742b4572cce6a3596914e4
romainsacchi <r_s@me.com>
2019-10-16 17:18:09 +0200

    Update

--- static/js/custom.js
+++ static/js/custom.js
@@ -103,6 +103,100 @@
 
 	new ClipboardJS('.btn');
 	$("#bgndVideo").YTPlayer();
+
+      var Messenger = function(el){
+      'use strict';
+      var m = this;
+
+      m.init = function(){
+        m.codeletters = "&#*+%?£@§$";
+        m.message = 0;
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        m.messages = [
+          'Are electric cars better than my old diesel car?',
+          'Is it time to consider fuel cell cars?',
+          'Will my driving affect the range of my electric car?',
+          'Find it out now with carculator.'
+        ];
+
+        setTimeout(m.animateIn, 100);
+      };
+
+      m.generateRandomString = function(length){
+        var random_text = '';
+        while(random_text.length < length){
+          random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        }
+
+        return random_text;
+      };
+
+      m.animateIn = function(){
+        if(m.current_length < m.messages[m.message].length){
+          m.current_length = m.current_length + 2;
+          if(m.current_length > m.messages[m.message].length) {
+            m.current_length = m.messages[m.message].length;
+          }
+
+          var message = m.generateRandomString(m.current_length);
+          $(el).html(message);
+
+          setTimeout(m.animateIn, 20);
+        } else {
+          setTimeout(m.animateFadeBuffer, 140);
+        }
+      };
+
+      m.animateFadeBuffer = function(){
+        if(m.fadeBuffer === false){
+          m.fadeBuffer = [];
+          for(var i = 0; i < m.messages[m.message].length; i++){
+            m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+          }
+        }
+
+        var do_cycles = false;
+        var message = '';
+
+        for(var i = 0; i < m.fadeBuffer.length; i++){
+          var fader = m.fadeBuffer[i];
+          if(fader.c > 0){
+            do_cycles = true;
+            fader.c--;
+            message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+          } else {
+            message += fader.l;
+          }
+        }
+
+        $(el).html(message);
+
+        if(do_cycles === true){
+          setTimeout(m.animateFadeBuffer, 50);
+        } else {
+          setTimeout(m.cycleText, 5000);
+        }
+      };
+
+      m.cycleText = function(){
+        m.message = m.message + 1;
+        if(m.message >= m.messages.length){
+          m.message = m.messages.length;
+        }
+
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        $(el).html('');
+
+        setTimeout(m.animateIn, 200);
+      };
+
+      m.init();
+    }
+
+    console.clear();
+    var messenger = new Messenger($('#messenger'));
 	
 })( jQuery );
 
