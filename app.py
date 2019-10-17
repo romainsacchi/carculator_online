8ee0fca5d046236e9fe8b5086d388eee3dfb19ff
romainsacchi <r_s@me.com>
2019-10-17 13:20:05 +0200

    Added translation files

--- app.py
+++ app.py
@@ -60,6 +60,4 @@ def send_email():
 
 @babel.localeselector
 def get_locale():
-    print(request.accept_languages.best_match(app.config['LANGUAGES']))
-    
     return request.accept_languages.best_match(app.config['LANGUAGES'])
