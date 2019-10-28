6614ce97f140e60544168f340e2e090ec35fdabd
romainsacchi <r_s@me.com>
2019-10-28 19:46:51 +0100

    Update

--- static/js/tool_page.js
+++ static/js/tool_page.js
@@ -1,3 +1,52 @@
+	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
+
+		// Cache selectors
+		var lastId,
+		topMenu = $(".mu-menu"),
+		topMenuHeight = topMenu.outerHeight()+13,
+		// All list items
+		menuItems = topMenu.find('a[href^=\\#]'),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function(){
+		  var item = $($(this).attr("href"));
+		  if (item.length) { return item; }
+		});
+
+		// Bind click handler to menu items
+		// so we can get a fancy scroll animation
+		menuItems.click(function(e){
+		  var href = $(this).attr("href"),
+		      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+22;
+		  jQuery('html, body').stop().animate({
+		      scrollTop: offsetTop
+		  }, 1500);
+		  e.preventDefault();
+		});
+
+		// Bind to scroll
+		jQuery(window).scroll(function(){
+		   // Get container scroll position
+		   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+		   // Get id of current scroll item
+		   var cur = scrollItems.map(function(){
+		     if ($(this).offset().top < fromTop)
+		       return this;
+		   });
+		   // Get the id of the current element
+		   cur = cur[cur.length-1];
+		   var id = cur && cur.length ? cur[0].id : "";
+
+		   if (lastId !== id) {
+		       lastId = id;
+		       // Set/remove active class
+		       menuItems
+		         .parent().removeClass("active")
+		         .end().filter("[href=\\#"+id+"]").parent().addClass("active");
+		   }
+		})
+
+
 
 
 (function()
