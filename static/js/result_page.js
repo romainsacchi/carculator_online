59ce95b73f6b1260efe73dd7c40b37aff00265f8
romainsacchi <r_s@me.com>
2019-12-07 20:12:50 +0100

    Update result_page.js

--- static/js/result_page.js
+++ static/js/result_page.js
@@ -1,24 +1,25 @@
-var list_unit = {
-        'agricultural land occupation':'square meter-year',
-        'climate change':'kg CO2-Eq',
-        'fossil depletion':'kg oil-Eq',
-        'freshwater ecotoxicity':'kg 1,4-DC.',
-        'freshwater eutrophication':'kg P-Eq',
-        'human toxicity':'kg 1,4-DC.',
-        'ionising radiation':'kg U235-Eq',
-        'marine ecotoxicity':'kg 1,4-DC.',
-        'marine eutrophication':'kg N-Eq',
-        'metal depletion':'kg Fe-Eq',
-        'natural land transformation':'square meter',
-        'ozone depletion':'kg CFC-11.',
-        'particulate matter formation':'kg PM10-Eq',
-        'photochemical oxidant formation':'kg NMVOC',
-        'terrestrial acidification':'kg SO2-Eq',
-        'terrestrial ecotoxicity':'kg 1,4-DC.',
-        'urban land occupation':'square meter-year',
-        'water depletion':'cubic meter',
-        'human noise':'Person-Pa.s',
-        };
+(function( $ ){
+    var list_unit = {
+            'agricultural land occupation':'square meter-year',
+            'climate change':'kg CO2-Eq',
+            'fossil depletion':'kg oil-Eq',
+            'freshwater ecotoxicity':'kg 1,4-DC.',
+            'freshwater eutrophication':'kg P-Eq',
+            'human toxicity':'kg 1,4-DC.',
+            'ionising radiation':'kg U235-Eq',
+            'marine ecotoxicity':'kg 1,4-DC.',
+            'marine eutrophication':'kg N-Eq',
+            'metal depletion':'kg Fe-Eq',
+            'natural land transformation':'square meter',
+            'ozone depletion':'kg CFC-11.',
+            'particulate matter formation':'kg PM10-Eq',
+            'photochemical oxidant formation':'kg NMVOC',
+            'terrestrial acidification':'kg SO2-Eq',
+            'terrestrial ecotoxicity':'kg 1,4-DC.',
+            'urban land occupation':'square meter-year',
+            'water depletion':'cubic meter',
+            'human noise':'Person-Pa.s',
+            };
 
     var data = {{data|safe}};
     var imp;
@@ -37,10 +38,10 @@ var list_unit = {
           $.pivotUtilities.plotly_renderers
         ),
         rendererOptions: {plotly: { yaxis: { title: list_unit['climate change']},
-        							title: {text: 'Climate change impacts, per vehicle-kilometer'}}},
+                                    title: {text: 'Climate change impacts, per vehicle-kilometer'}}},
 
         onRefresh: function(config){
-        	imp = config['inclusions']['impact category'][0]
+            imp = config['inclusions']['impact category'][0]
             }
       });
 
@@ -59,28 +60,29 @@ var list_unit = {
           $.pivotUtilities.plotly_renderers
         ),
         rendererOptions: {plotly: { yaxis: { title: '€ per vkm'},
-        							title: {text: 'Ownership cost over life cycle, per vehicle-kilometer'}}},
+                                    title: {text: 'Ownership cost over life cycle, per vehicle-kilometer'}}},
       });
+  }
 
-     function share_results(){
-        $.notify({
-            icon: 'glyphicon glyphicon-warning-sign',
-            message: "Your results are saved for one month. To share them, simply share the link of this page.."
+ function share_results(){
+    $.notify({
+        icon: 'glyphicon glyphicon-warning-sign',
+        message: "Your results are saved for one month. To share them, simply share the link of this page.."
+    },
+    {
+        placement: {
+            from: "top",
+            align: "center"
         },
-        {
-            placement: {
-                from: "top",
-                align: "center"
-            },
-            type:'success'
+        type:'success'
+    },
+    {
+        animate: {
+            enter: 'animated bounceInDown',
+            exit: 'animated bounceOutUp'
         },
-        {
-            animate: {
-                enter: 'animated bounceInDown',
-                exit: 'animated bounceOutUp'
-            },
 
-        }
-        );
+    }
+    );
 
-     }
+ };
