7eee476bd4d82e8b66d79ca032076ea76e9128cb
romainsacchi <r_s@me.com>
2019-12-31 11:01:53 +0100

    Fix

--- app/worker.py
+++ app/worker.py
@@ -5,9 +5,15 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'None')
+redis_url = os.getenv('REDISCLOUD_URL', 'None')
+redis_pass = os.getenv('REDISTOGO_PASS', 'None')
 
-conn = redis.from_url(redis_url)
+#conn = redis.from_url(redis_url)
+
+conn = redis.Redis(
+    host=redis_url,
+    port=18718,
+    password=redis_pass)
 
 
 if __name__ == '__main__':
