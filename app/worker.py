9c4f9d09de1734a529aa6982e59f85800b7147fe
romainsacchi <r_s@me.com>
2019-12-31 11:25:06 +0100

    Fix

--- app/worker.py
+++ app/worker.py
@@ -1,18 +1,13 @@
 import os
-import urlparse
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
-conn = redis.Redis(host=url.hostname, port=url.port, password=url.password)
-
-
-#redis_url = os.getenv('REDISCLOUD_URL', 'None')
+redis_url = os.getenv('REDISCLOUD_URL', 'None')
 #redis_pass = os.getenv('REDISTOGO_PASS', 'None')
 
-#conn = redis.from_url(redis_url)
+conn = redis.from_url(redis_url)
 
 #conn = redis.Redis(
 #    host=redis_url,
