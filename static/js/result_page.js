a114d3ae18aa1b0558586a818eeb9f2d3e4fe628
romainsacchi <r_s@me.com>
2020-01-02 15:03:10 +0100

    Fix

--- static/js/result_page.js
+++ static/js/result_page.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
      console.log("check!")
-     var progressSelector = $(".progress-wrap");
-     progressSelector.each(function(){
-     var getPercent = $(this).attr("data-progresspercent");
-     var getSpeed = parseInt($(this).attr("data-speed"));
-     var getColor = $(this).attr("data-color");
-     var getHeight = $(this).attr("data-height");
-     var getWidth = $(this).attr("data-width");
-     $(this).css({"height":getHeight,"width":getWidth});
-     $(this).find(".progress-bar").css({"background-color":"#"+getColor}).animate({ width:getPercent+'%' },getSpeed)
-     });
+     
 });
