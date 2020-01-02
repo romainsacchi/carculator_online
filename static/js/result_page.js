8dbcdb3e0f7154ceed99cd652fab14331f36b9eb
romainsacchi <r_s@me.com>
2020-01-02 15:08:21 +0100

    Fix

--- static/js/result_page.js
+++ static/js/result_page.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
      console.log("check!")
-     
+
+     function generate_progress_bars(filter){
+        if (filter == "climate change"){
+
+        };
+        if (filter == "cost"){
+
+        };
+        if (filter == "fossil depletion"){
+
+        };
+     };
 });
