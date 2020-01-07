96d084afa5c3b297beb8e990f15d123feea02643
romainsacchi <r_s@me.com>
2020-01-07 15:14:18 +0100

    Update calculation.py

--- app/calculation.py
+++ app/calculation.py
@@ -326,7 +326,8 @@ class Calculation():
                     for cat in range(0, len(cost_category)):
                         if cost_category[cat] == "total":
                             arr_benchmark.append(["cost", size[s], powertrain[pt], year[y], 1 / data_cost[0, s, pt, y, cat]])
-                            dict_scatter[(size[s], powertrain[pt], year[y])].append([data_cost[0, s, pt, y, cat]])
+                            k = size[s] + ", " + powertrain[pt] + ", " + str(year[y])
+                            dict_scatter[k].append([data_cost[0, s, pt, y, cat]])
 
                         list_res_costs.append([data_cost[0, s, pt, y, cat], size[s], powertrain[pt], year[y], cost_category[cat]])
 
@@ -365,7 +366,8 @@ class Calculation():
                         if imp == 6:
                             arr_benchmark.append(["climate change", size[s], powertrain[pt], year[y],
                                              1 / data[imp, s, pt, y, :,0].sum()])
-                            dict_scatter[(size[s], powertrain[pt], year[y])].append(data[imp, s, pt, y, :,0].sum())
+                            k = size[s] + ", " + powertrain[pt] + ", " + str(year[y])
+                            dict_scatter[k].append(data[imp, s, pt, y, :,0].sum())
                         if imp == 7:
                             arr_benchmark.append(["fossil depletion", size[s], powertrain[pt], year[y],
                                              1 / (data[imp, s, pt, y, :, 0].sum() * 0.755)]) # 0.755 kg/L gasoline
