c10bc13476a6091c822d8a75ac261d239e5de072
romainsacchi <r_s@me.com>
2020-01-14 14:48:28 +0100

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from .calculation import Calculation
 ROOT = os.path.join(os.path.abspath(os.pardir), "carculator_online");
 TEMPLATES_DIR = os.path.join(ROOT, "templates")
 STATIC_DIR = os.path.join(ROOT, "static")
-MIGRATION_DIR = os.path.join(ROOT, "app/migrations")
+MIGRATION_DIR = os.path.join(ROOT, "migrations")
 
 # Instantiate Flask app
 app = Flask(__name__,
@@ -52,7 +52,7 @@ else:
 # Initiate database
 db = SQLAlchemy(app)
 
-migrate = Migrate(app, db, directory = MIGRATION_DIR)
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 
 # Setup logger to log errors by email
 auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
