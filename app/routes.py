a1917da86765ec04d211c423c3f51e6353b145ca
romainsacchi <r_s@me.com>
2020-05-15 14:59:49 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -425,7 +425,7 @@ def get_results():
     job = q.enqueue_call(
         func=app.calc.process_results,
         args=(d, lang, job_id),
-        result_ttl=120,
+        result_ttl=300,
         job_id=job_id,
     )
 
