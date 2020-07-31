d1cc20cf311e47fe0fbcc1bb8dc31f7a67bf73b1
romainsacchi <r_s@me.com>
2020-07-31 11:03:59 +0200

    Version bump

--- static/js/result_page.js
+++ static/js/result_page.js
@@ -68,7 +68,7 @@
      'Primary fuel', 'Primary fuel share', 'Secondary fuel', 'Secondary fuel share']);
 
     data_to_parse.unshift([]);
-    data_to_parse.unshift(['carculator online 1.1.0', 'carculator 1.1.8', 'https://carculator.psi.ch']);
+    data_to_parse.unshift(['carculator online 1.1.0', 'carculator 1.1.9', 'https://carculator.psi.ch']);
 
     var csv = Papa.unparse(data_to_parse);
     var csvData = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
