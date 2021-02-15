63dacf781886a2a65a1fc60d284f737297abf8c4
romainsacchi <r_s@me.com>
2021-02-15 15:15:47 +0100

    U

--- static/js/result_page.js
+++ static/js/result_page.js
@@ -54,7 +54,7 @@ function export_results()
      'Primary fuel', 'Primary fuel share', 'Secondary fuel', 'Secondary fuel share']);
 
     data_to_parse.unshift([]);
-    data_to_parse.unshift(['carculator online 1.1.5', 'carculator 1.3.2', 'https://carculator.psi.ch']);
+    data_to_parse.unshift(['carculator online 1.1.7', 'carculator 1.3.9', 'https://carculator.psi.ch']);
 
     var csv = Papa.unparse(data_to_parse);
     var csvData = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
