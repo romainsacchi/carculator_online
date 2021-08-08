7d8bd6c1466f730fb298d8f037f78fe3d1927680
romainsacchi <r_s@me.com>
2021-08-08 22:53:04 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -104,7 +104,6 @@ mail = Mail(app)
 from app import email_support
 from app import routes, models
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
