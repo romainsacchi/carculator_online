3f235d325681d2432cf292bf2e7db225e4f6b6c1
romainsacchi <r_s@me.com>
2021-08-09 09:54:28 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -337,7 +337,7 @@ def tool_page(country):
                             "type": "NMC-111",
                             "origin": "CN",
                             "energy battery mass": [400],
-                            "battery cell energy density, NMC-111": [0.2],
+                            "battery cell energy density": [0.2],
                             "battery lifetime kilometers": [200000],
                         }
                     }
