6501ef9861bbfcdf99b31139d20527b6402317d0
romainsacchi <r_s@me.com>
2023-02-12 19:13:10 +0100

    Update

--- app/worker.py
+++ app/worker.py
@@ -14,21 +14,6 @@ try:
         socket_keepalive=True
     )
 
-    # Create a PubSub instance
-    p = conn.pubsub()
-
-    # Subscribe to the channel "test"
-    p.subscribe('test')
-
-    # Create a function that will call `check_health` every 5 seconds
-    def redis_auto_check(p):
-        t = threading.Timer(5, redis_auto_check, [p])
-        t.start()
-        p.check_health()
-
-    # Call the redis_auto_check function
-    redis_auto_check(p)
-
 except ValueError:
     print("Cannot connect to Redis server.")
     conn = None
