da324879120e342f703edb3fd7e5d2d7383c9deb
romainsacchi <r_s@me.com>
2024-12-16 17:30:17 +0100

    EODC

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@
 TESTING = True
 DEBUG = True
 FLASK_DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://bee698db94fa6e:02c34128@us-cdbr-iron-east-05.cleardb.net/heroku_9bb2a3349ea4243'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:NwwQb14QsGcnASCviwMSEcJCrSdqOcCj@cnqvnz.stackhero-network.com:7930/root?useSSL=true&requireSSL=true'
 MAIL_USERNAME = 'carculator@psi.ch'
 MAIL_PASSWORD = 'xxxxxxxx'
 MAIL_SERVER = 'smtp.gmail.com'
