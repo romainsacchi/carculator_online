0c226cc3f8e129fe24e3dade59087aace3272d8e
romainsacchi <r_s@me.com>
2024-12-16 17:44:34 +0100

    EODC

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@
 TESTING = True
 DEBUG = True
 FLASK_DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:NwwQb14QsGcnASCviwMSEcJCrSdqOcCj@cnqvnz.stackhero-network.com:7930'
 MAIL_USERNAME = 'carculator@psi.ch'
 MAIL_PASSWORD = 'xxxxxxxx'
 MAIL_SERVER = 'smtp.gmail.com'
